139854cd65b7d0c058f33cf4754ef559fd815c65
Eric <ehevelone@gmail.com>
2026-03-01 08:14:12 -0600

    Fix main routing and required params

--- lib/main.dart
+++ lib/main.dart
@@ -5,67 +5,54 @@ import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-// Core screens
+// SCREENS
 import 'screens/landing_screen.dart';
 import 'screens/splash_screen.dart';
-import 'screens/registration_screen.dart';
 import 'screens/login_screen.dart';
-import 'screens/account_setup_screen.dart';
-import 'screens/menu_screen.dart';
-import 'screens/agent_menu.dart';
-import 'screens/agent_registration_screen.dart';
 import 'screens/agent_login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/account_setup_screen.dart';
+import 'screens/agent_registration_screen.dart';
 import 'screens/agent_setup_screen.dart';
 import 'screens/terms_user_screen.dart';
 import 'screens/terms_agent_screen.dart';
 import 'screens/logo_screen.dart';
+import 'screens/menu_screen.dart';
+import 'screens/agent_menu.dart';
 import 'screens/my_agent_user.dart';
 import 'screens/my_agent_agent.dart';
 import 'screens/emergency_screen.dart';
 import 'screens/emergency_view.dart';
 
 // PROFILE
-import 'screens/profile_agent_screen.dart';
 import 'screens/profile_user_screen.dart';
+import 'screens/profile_agent_screen.dart';
 import 'screens/edit_profile.dart';
 import 'screens/profile_picker.dart';
 import 'screens/new_profile_screen.dart';
 
-// Medical / insurance data
+// MEDICAL
 import 'screens/meds_screen.dart';
 import 'screens/doctors_screen.dart';
 import 'screens/doctors_view.dart';
 import 'screens/insurance_policies.dart';
-import 'screens/insurance_policy_view.dart';
-import 'screens/insurance_policy_form.dart';
 import 'screens/insurance_cards.dart';
-import 'screens/insurance_card_detail.dart';
 import 'screens/insurance_cards_menu_ios.dart';
 
 // HIPAA
 import 'screens/hipaa_form_screen.dart';
 
-// Utilities
+// UTIL
 import 'screens/scan_card.dart';
 
-// Password reset
+// PASSWORD
 import 'screens/request_reset_screen.dart';
 import 'screens/reset_password_screen.dart';
 import 'screens/agent_request_reset_screen.dart';
 import 'screens/agent_reset_password_screen.dart';
 
-import 'models.dart';
-
 final GlobalKey<NavigatorState> navigatorKey =
     GlobalKey<NavigatorState>();
-
-@pragma('vm:entry-point')
-Future<void> firebaseMessagingBackgroundHandler(
-    RemoteMessage message) async {
-  try {
-    await Firebase.initializeApp();
-  } catch (_) {}
-}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -77,14 +64,11 @@ Future<void> main() async {
 
     try {
       await Firebase.initializeApp();
-      FirebaseMessaging.onBackgroundMessage(
-        firebaseMessagingBackgroundHandler,
-      );
     } catch (_) {}
 
     runApp(const VitaLinkApp());
   }, (error, stack) {
-    debugPrint("🔥 ZONED ERROR: $error");
+    debugPrint("ZONED ERROR: $error");
     debugPrint(stack.toString());
   });
 }
@@ -99,7 +83,6 @@ class VitaLinkApp extends StatelessWidget {
       title: 'VitaLink',
       debugShowCheckedModeBanner: false,
       initialRoute: '/splash',
-
       routes: {
         '/landing': (context) => const LandingScreen(),
         '/splash': (context) => const SplashScreen(),
@@ -107,13 +90,13 @@ class VitaLinkApp extends StatelessWidget {
         '/login': (context) => const LoginScreen(),
         '/agent_login': (context) => const AgentLoginScreen(),
 
-        '/terms_user': (context) => const TermsUserScreen(),
-        '/terms_agent': (context) => const TermsAgentScreen(),
-
         '/registration': (context) => const RegistrationScreen(),
         '/account_setup': (context) => const AccountSetupScreen(),
         '/agent_registration': (context) => const AgentRegistrationScreen(),
         '/agent_setup': (context) => const AgentSetupScreen(),
+
+        '/terms_user': (context) => const TermsUserScreen(),
+        '/terms_agent': (context) => const TermsAgentScreen(),
 
         '/logo': (context) => const LogoScreen(),
         '/menu': (context) => const MenuScreen(),
@@ -127,7 +110,6 @@ class VitaLinkApp extends StatelessWidget {
 
         '/my_profile_user': (context) => const ProfileUserScreen(),
         '/my_profile_agent': (context) => const ProfileAgentScreen(),
-
         '/edit_profile': (context) => EditProfile(),
         '/profile_picker': (context) => ProfilePicker(),
         '/new_profile': (context) => const NewProfileScreen(),
@@ -151,35 +133,6 @@ class VitaLinkApp extends StatelessWidget {
             const AgentRequestResetScreen(),
         '/agent_reset_password': (context) =>
             const AgentResetPasswordScreen(),
-      },
-
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/insurance_policy_view':
-            final index = settings.arguments as int;
-            return MaterialPageRoute(
-              builder: (_) =>
-                  InsurancePolicyView(index: index),
-            );
-
-          case '/insurance_policy_form':
-            final args = settings.arguments as Map<String, dynamic>;
-            return MaterialPageRoute(
-              builder: (_) => InsurancePolicyForm(
-                policy: args['policy'],
-                allPolicies: args['allPolicies'],
-              ),
-            );
-
-          case '/insurance_card_detail':
-            final card =
-                settings.arguments as InsuranceCard;
-            return MaterialPageRoute(
-              builder: (_) =>
-                  InsuranceCardDetail(card: card),
-            );
-        }
-        return null;
       },
     );
   }
